7577a4048cdd7e29270396807284e59ad39812e7
Pertsaa <65128229+Pertsaa@users.noreply.github.com>
2022-05-02 19:43:05 +0300

    Add rasa weather api example

---
+++ actions/actions.py
@@ -0,0 +1,32 @@
+import random
+from datetime import datetime
+from typing import Text, List, Any, Dict
+
+from rasa_sdk import Tracker, Action
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+class ActionFetchWeather(Action):
+   def name(self) -> Text:
+      return "action_fetch_weather"
+
+   def run(self,
+           dispatcher: CollectingDispatcher,
+           tracker: Tracker,
+           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+      # Use today as date if time is not set or time has passed.
+      time_string = tracker.get_slot('time')
+      time = datetime.fromisoformat(time_string)
+      if time is None or time < datetime.now(time.tzinfo):
+        time = datetime.now()
+      formatted_date = time.strftime("%m/%d/%Y")
+
+      # Here we are returning mock weather data. In a real application this
+      # would be an API call to a weather API using the time slot.
+      weather_list = ["sunny", "cloudy", "windy", "rainy"]
+      result = random.choice(weather_list)
+
+      # Set the weather slot to match the result of the fetched weather data.
+      # Set the formatted_date slot so the chatbot can use a more readable date.
+      return [SlotSet("weather", result), SlotSet("formatted_date", formatted_date)]
